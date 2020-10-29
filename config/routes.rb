b44b816fca2bedc6fe02cf7062c8a41b047b7eca
root <ashkanebtekari@gmail.com>
2020-10-29 12:10:45 -0400

    added authenticate routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   # Main Root
   root 'pages#index', as: 'root'
 
-  # Routes
+  # Main Routes
   get 'explore' => 'pages#explore', as: 'explore'
   get 'signup' => 'pages#signup', as: 'signup'
   get 'login' => 'pages#login', as: 'login'
+
+  # Authentication Routes
+  post 'users' => 'pages#create', as: 'create'
 
 end
