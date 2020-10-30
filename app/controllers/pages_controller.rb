42efbb8dddc268ecab58ec5fd53b1c725500180f
root <ashkanebtekari@gmail.com>
2020-10-30 14:34:27 -0400

    completed the signup method and create method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,38 @@
 class PagesController < ApplicationController
-
   # Main Page
   def index
     @logo = 'BookTalk'
   end
 
   # Explore Page
-  def explore
-
-  end
+  def explore; end
 
   # SignUp Page
-  def signup
+  def signup; end
 
+  # SignUp Creation
+  def create
+    user = User.new(
+      fname: params[:fname],
+      lname: params[:lname],
+      email: params[:email],
+      password: params[:password]
+    )
+
+    if user
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully Created The User !'
+      puts('Yaaaayyyy !')
+      redirect_to root_path
+
+    else
+      flash[:warning] = 'Could Not Create The User !'
+      puts('NOoooooooooooo')
+      redirect_to signup_path
+
+    end
   end
 
   # Login Page
-  def login
-
-  end
-
+  def login; end
 end
