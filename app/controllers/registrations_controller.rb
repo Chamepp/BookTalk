7eb3507f8961be517eba81d53c74eb152ec90e28
root <ashkanebtekari@gmail.com>
2020-11-13 05:49:04 -0500

    added more complete json status

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,29 @@
 class RegistrationsController < ApplicationController
   
+  # SignUp Page
+  def signup; end
+
+  # SignUp Creation
+  def create
+    user = User.create!(
+      fname: params[:fname],
+      lname: params[:lname],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if user
+      session[:user_id] = user.id
+      render json: {
+        Status: :created,
+        User_Id: user.id,
+        Firstname: user.fname,
+        Lastname: user.lname
+      }
+
+    else
+      render json: { status: 500 }
+    end
+  end
 end
