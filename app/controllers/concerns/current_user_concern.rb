174968a6a86015a08a18a12f0e26d2295fef427a
root <ashkanebtekari@gmail.com>
2020-11-17 12:48:10 -0500

    created and completed the user concern

---
+++ app/controllers/concerns/current_user_concern.rb
@@ -0,0 +1,14 @@
+module CurrentUserConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_current_user
+  end
+
+
+  def set_current_user
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
+  end
+end
