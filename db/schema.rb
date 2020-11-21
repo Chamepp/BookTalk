a88a2016ca41cfa3b91d21c41d03680a1e297225
root <ashkanebtekari@gmail.com>
2020-11-21 12:35:12 -0500

    schema update for the cards table migration model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_155530) do
+ActiveRecord::Schema.define(version: 2020_11_19_163951) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string "cardname"
+    t.integer "cardprice"
+    t.string "carddes"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "fname"
