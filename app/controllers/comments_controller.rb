dc4cba752cf6d6007afe192d0f928629daf47bbf
root <ashkanebtekari@gmail.com>
2020-11-25 06:22:10 -0500

    completed the commenting ability

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,17 @@
 class CommentsController < ApplicationController
+
+  # Create Comments
+  def create
+    @card = Card.find(params[:card_id])
+    @comment = @card.comments.create([comment_params])
+
+    redirect_to card_path(@card)
+
+  end
+
+
+  # Comments Params
+  public def comment_params
+    params.require(:comment).permit(:commentbody)
+  end
 end
