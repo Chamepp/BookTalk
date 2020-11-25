0fd64b151815da947ee62c96f707987395d89e88
root <ashkanebtekari@gmail.com>
2020-11-25 06:08:24 -0500

    added cards validation methods for cards parameters

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,9 @@
 class Card < ApplicationRecord
+  has_many :comments
+  validates :cardname, presence: true, length: { minimum: 5 }
+  validates :cardprice, presence: true, length: { minimum: 1 }
+  validates :carddes, presence: true, length: { minimum: 30, maximum: 40 }
+  validates :cardgen, presence: true
+  validates :cardpage, presence: true, length: { minimum: 1 }
+  validates :cardyear, presence: true, length: { minimum: 1 }
 end
