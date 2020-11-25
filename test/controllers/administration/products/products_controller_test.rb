33e519fb70372c2cb99b6c5c02797a8c5a2cf4a2
root <ashkanebtekari@gmail.com>
2020-11-25 06:26:40 -0500

    comment test controllers

---
+++ test/controllers/administration/products/products_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Administration::Products::ProductsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/controllers/administration/products_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Administration::ProductsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
