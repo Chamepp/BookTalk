7530f48fe11a87f8449767e396df584ccb689ac1
root <ashkanebtekari@gmail.com>
2020-11-28 03:11:39 -0500

    created the routes for editing and destroying

--- config/routes.rb
+++ config/routes.rb
@@ -18,16 +18,23 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy', as: 'destroy-login'
 
   # Admin Sources
-  get 'admin' => 'administration#administration', as: 'admin'
-  get 'admin/products' => 'administration#control_products', as: 'control_product'
-  get 'admin/customers' => 'cards#control_customers', as: 'control_customers'
-  post 'admin/products' => 'cards#create'
+  get 'admin' => 'admin#admin', as: 'admin'
+  post 'admin' => 'admin#create', as: 'admin_login'
+  get 'admin/logout' => 'admin#destroy', as: 'admin_destroy_login'
+  get 'admin/dashboard' => 'administration#dashboard', as: 'dashboard'
+  get 'admin/dashboard/products' => 'administration#control_products', as: 'control_product'
+  get 'admin/dashboard/customers' => 'administration#control_customers', as: 'control_customers'
+
+  # Creation Sources
+  post 'admin/dashboard/products' => 'cards#create'
   get 'cards' => 'cards#show'
-
-
+  get 'cards' => 'cards#edit', as: 'card_edit'
+  get 'cards' => 'cards#destroy', as: 'card_delete'
+  put 'cards' => 'cards#update', as: 'card_update'
 
   resources :cards do
     resources :comments
   end
 
 end
+
