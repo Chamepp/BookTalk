87c8f33fcded86d4209756620bcb8a3e433d200b
root <ashkanebtekari@gmail.com>
2020-12-20 07:12:16 -0500

    added the cardpic parameter to the model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ApplicationRecord
   has_many :comments, dependent: :delete_all
+  has_one_attached :cardpic
   validates :cardname, presence: true, length: { minimum: 2 }
   validates :carddes, presence: true, length: { minimum: 50, maximum: 200 }
   validates :cardprice, presence: true
