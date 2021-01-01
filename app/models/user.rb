c32ae861335640be1a932a7536198b5e73c74b4b
root <ashkanebtekari@gmail.com>
2021-01-01 14:42:56 -0500

    added dependencies to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+  has_many :cards, dependent: :destroy
+  has_many :books
+  has_many :books_additions, through: :books, source: :card
   has_one_attached :avatar
   validates_presence_of :email
   validates_uniqueness_of :email
