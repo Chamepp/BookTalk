b23f28f03fe8e77aee88282bec4c2d8842a99b39
root <ashkanebtekari@gmail.com>
2021-01-03 15:18:46 -0500

    update4d the database file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_101134) do
+ActiveRecord::Schema.define(version: 2020_12_21_105823) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2020_12_17_101134) do
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
