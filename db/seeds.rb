56f04dd15c8701df7d197ba39ef42bc04ad55488
System Administrator <ashkanebtekari@gmail.com>
2021-10-16 10:44:21 +0330

    completed seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,44 @@
 
 Admin.create!(:username => 'admin', :password => 'admin')
 puts("Created Admins ...")
+
 User.create!(:fname => 'Lorem', :lname => 'Ipsum', :email => 'lorem@gmail.com', :password => 'lorem')
 User.create!(:fname => 'Cool', :lname => 'Joe', :email => 'cool@gmail.com', :password => 'cool')
 User.create!(:fname => 'Man', :lname => 'Found', :email => 'man@gmail.com', :password => 'man')
 User.create!(:fname => 'Dude', :lname => 'Guy', :email => 'guy@gmail.com', :password => 'guy')
 puts("Created Users ...")
+
+Card.create!(
+    :cardname => "The Willoughby's",
+    :cardprice => 299,
+    :carddes => "The Willoughby's is a great book for teenagers who are looking for new information",
+    :cardgen => "comedy",
+    :cardpage => "486",
+    :cardyear => 2020,
+    :carddownload => 400000,
+    :cardpremium => "premium"
+)
+
+Card.create!(
+    :cardname => "The Gradle Cookbook",
+    :cardprice => 499,
+    :carddes => "The Gradle Cookbook is for technical purposes managed by developers",
+    :cardgen => "science",
+    :cardpage => "532",
+    :cardyear => 2021,
+    :carddownload => 700000,
+    :cardpremium => "premium"
+)
+
+Card.create!(
+    :cardname => "The Red Rose",
+    :cardprice => 299,
+    :carddes => "The Red Rose is a drama book based on a true story by Jason William",
+    :cardgen => "drama",
+    :cardpage => "87",
+    :cardyear => 1998,
+    :carddownload => 10000,
+    :cardpremium => "free"
+)
+puts('Created Books ...')
+puts('Operation Done Successfuly !')
