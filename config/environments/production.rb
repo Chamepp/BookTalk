7aa7f3144636d95cee5d7b667eb7ee0f5600a16b
System Administrator <ashkanebtekari@gmail.com>
2021-11-02 13:00:49 +0330

    assets production update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
